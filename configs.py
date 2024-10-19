02e0b2ce35de099a3926103283ea1e789e9786be
abhilaskar <147780036+abhilaskar@users.noreply.github.com>
2024-10-19 20:02:00 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -3,14 +3,14 @@
 import os
 
 class Config(object):
-	API_ID = int(os.environ.get("API_ID", "0"))
-	API_HASH = os.environ.get("API_HASH")
-	BOT_TOKEN = os.environ.get("BOT_TOKEN")
-	BOT_USERNAME = os.environ.get("BOT_USERNAME")
+	API_ID = int(os.environ.get("API_ID", "24872345"))
+	API_HASH = os.environ.get("API_HASH", "e4d28d51a26681f692a24d3ae7cb8d0c")
+	BOT_TOKEN = os.environ.get("BOT_TOKEN", "7882058840:AAGIJivkzXOBWv-2o0aBD-dSJEU-NxNb49k")
+	BOT_USERNAME = os.environ.get("BOT_USERNAME", "bhiencoderbot")
 	DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-100"))
 	SHORTLINK_URL = os.environ.get('SHORTLINK_URL')
 	SHORTLINK_API = os.environ.get('SHORTLINK_API')
-	BOT_OWNER = int(os.environ.get("BOT_OWNER", "1445283714"))
+	BOT_OWNER = int(os.environ.get("BOT_OWNER", "1373409212"))
 	DATABASE_URL = os.environ.get("DATABASE_URL")
 	UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "")
 	LOG_CHANNEL = os.environ.get("LOG_CHANNEL", None)
